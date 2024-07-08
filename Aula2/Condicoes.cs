44d35c1663af75ec2bacc76f0ff3701c0288b7e4
Sabrina Comin Nart <sabrinacominnart@gmail.com>
2024-07-07 22:13:12 -0300

    Anotacoes

---
+++ Aula2/Condicoes.cs
@@ -0,0 +1,40 @@
+﻿
+        /*
+        Console.WriteLine("Digite sua idade: ");
+        int? idade = int.Parse(Console.ReadLine());
+
+        //Console.WriteLine("Sua idade é: ");
+        //Console.WriteLine(idade);
+
+        if (idade < 18)
+        {
+            //Console.WriteLine("Você é menor de idade e não pode dirigir! - Sua idade é: {0} + {1} + {2}", idade, idade, 23);
+            Console.WriteLine("Você é menor de idade e não pode dirigir! - Sua idade é: " + idade + "\n"); 
+        } else if (idade > 18)
+        {
+            Console.WriteLine("Você é maior de idade e pode dirigir");
+        } else
+        {
+            Console.WriteLine("Você está na carteira provisória");
+        }
+        *
+        //---------------------------------------------------------------------
+        
+        Console.WriteLine("Digite uma valor: ");
+        decimal valor = decimal.Parse(Console.ReadLine());
+
+        Console.WriteLine("Seu valor é: ");
+        Console.WriteLine(valor);
+        
+        //----------------------------------------------------------------------
+        /*
+        if
+        for    // Para cada coisa que acontecer, vou fazer outra coisa
+        while  // Enquanto uma situação for verdadeira, vou fazer outra coisa
+        switch //Para determinada informação, caso seja um valor, vou ver uma coisa
+        foreach //Para cada elemento dentro de uma lista - relação de vários objetos
+        */
+        //
+
+        // < > = ! <= >=
+
---
+++ Aula2/Lacos.cs
@@ -0,0 +1,32 @@
+﻿/*
+ 
+    for    // Para cada coisa que acontecer, vou fazer outra coisa
+    Sei o final - Quantas vezes vou repetir, até chegar na condição estabelecida
+
+
+    int contador2 = 0;
+
+    for (int contador = 0; contador < 5; contador++)
+    {
+        Console.WriteLine("Valor: " + contador);
+    }
+
+    //////////////////////////////////////////////
+
+    //While = Repetir enquanto uma condição não for aceita
+
+    while (contador2 < 10) //Entra e tem que sair - Algumas condições não são atendidas e vai executar eternamente
+    {
+        Console.WriteLine("Valores: " + contador2);
+        contador2++;
+    }
+
+    //--
+
+    do
+    { //Executa no mínimo uma fez e só ao final verificará se atende a validação e sairá
+        Console.WriteLine("Valores: " + contador2);
+        contador2++;
+    } while (contador2 < 10);
+
+*/
